8c1c80584bd026620194499e28d35e2b446f8fdc
sebban44 <51441314+sebban44@users.noreply.github.com>
2024-09-18 13:37:25 +0200

    Update fpl.R

--- fpl.R
+++ fpl.R
@@ -15,20 +15,20 @@ teams <- teams %>% select(id,team_name=short_name)
 players <- inner_join(players,position,by=c("element_type"="id"))
 players <- inner_join(players, teams, by=c("team"="id"))
 gk_list <- players %>% filter(pos == "GKP")
-player_list <- player_list %>% filter(!pos == "GKP")
+player_list <- players %>% filter(!pos == "GKP")
 
 player_list <- players %>% select(id, team_name, pos, name=web_name, starts, minutes, form,
                                   xG=expected_goals_per_90, xG_inv=expected_goal_involvements_per_90,
                                   xA=expected_assists_per_90,xGA=expected_goals_conceded_per_90,
                                   bonus,bps,influence,creativity,threat,ict_index,
                                   ppg=points_per_game,total_pts=total_points,
-                                  status, sel=selected_by_percent,news)
+                                  status, sel=selected_by_percent,news,cost=now_cost)
 
 gk_list <- gk_list %>% select(id, team_name, pos, name=web_name, starts, minutes, form,
                                   saves=saves_per_90,xGA=expected_goals_conceded_per_90,
                                   bonus,bps,influence,creativity,threat,ict_index,
                                   ppg=points_per_game,total_pts=total_points,
-                                  status, sel=selected_by_percent,news)
+                                  status, sel=selected_by_percent,news,cost=now_cost)
 
 player_list <- player_list %>% mutate(pts = (total_pts / minutes) * 90)
 gk_list <- gk_list %>% mutate(pts = (total_pts / minutes) * 90)
